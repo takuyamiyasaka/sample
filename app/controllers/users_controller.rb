3f238b3bf11b3ae7dd24dc5f5540faf03e01f978
toirai <larsalex8@gmail.com>
2020-03-29 20:15:22 +0900

    change rc 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index,:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def new
   	@user = User.new
+  end
+
+  def index
   end
 
   def show
@@ -19,9 +24,34 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
   private
 
   def user_params
   	params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
